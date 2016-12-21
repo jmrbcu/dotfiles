c8de51ccbf5a1a9a78f846352439edbef2972548
Jose Rodriguez <jrodriguez@interactivetel.com>
2016-12-20 22:36:07 -0600

    Full code refactor
    - Added generic install function
    - Added support for vim
    - Refactored most of the functions

--- install.py
+++ install.py
@@ -4,7 +4,6 @@ import platform
 import shutil
 import shlex
 import subprocess
-import fnmatch
 
 
 def _wrap_with(code):
@@ -32,15 +31,6 @@ blue = _wrap_with('34')
 magenta = _wrap_with('35')
 cyan = _wrap_with('36')
 white = _wrap_with('37')
-
-light_gray = _wrap_with('30')
-light_red = _wrap_with('31')
-light_green = _wrap_with('32')
-light_yellow = _wrap_with('33')
-light_blue = _wrap_with('34')
-light_magenta = _wrap_with('35')
-light_cyan = _wrap_with('36')
-light_white = _wrap_with('37')
 
 
 class CalledProcessError(Exception):
@@ -73,80 +63,136 @@ def check_output(*popenargs, **kwargs):
     return output
 
 
-def is_zsh_installed():
-    with open('/etc/shells') as f:
-        return 'zsh' in f.read()
+def init_child_process():
+    os.setpgrp()
+    os.umask(022)
 
 
-def install_zsh():
+def is_program_installed(name):
+    try:
+        check_output('which {0}'.format(name), shell=True, stderr=subprocess.STDOUT)
+        return True
+    except CalledProcessError:
+        return False
+
+
+def install_program(name):
     system = platform.system().lower()
+
     if 'darwin' in system:
-        cmd = 'brew install zsh'
+        cmd = 'brew install {0}'.format(name)
     elif 'linux' in system:
         dist = platform.dist()[0].lower()
         if dist in ('ubuntu', 'debian'):
-            cmd = 'sudo apt-get install -y zsh'
+            cmd = 'sudo apt-get install -y {0}'.format(name)
         elif dist == 'centos':
-            cmd = 'sudo yum install -y zsh'
+            cmd = 'sudo yum install -y {0}'.format(name)
         else:
-            raise RuntimeError(red('Cannot determine the system type, cannoy install zsh.'))
+            raise RuntimeError(red('Cannot determine the system type, cannot install zsh.'))
     else:
         raise RuntimeError(red('Cannot determine the system type, cannot install zsh.'))
 
     try:
-        print yellow(check_output(shlex.split(cmd)))
+        check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     except CalledProcessError as e:
-        print '{0}\n{1}'.format(red(e, True), yellow(e.output, True))
+        print '{0}\n{1}'.format(red(e, True), green(e.output, True))
 
 
 def install_ohmyzsh():
-    def init_child_process():
-        os.setpgrp()
-        os.umask(022)
+    print green('Installing: {0}'.format(red('oh-my-zsh', True)), True)
 
     home = os.getenv('HOME')
-    if not home:
-        raise RuntimeError(red('Cannot find home directory, exiting.'))
-
-    if not is_zsh_installed():
-        install_zsh()
+    current = os.path.abspath(os.path.dirname(__file__))
+    if not is_program_installed('zsh'):
+        install_program('zsh')
 
     zsh_dir = os.path.join(home, '.oh-my-zsh')
+    backup = os.path.join(home, '.oh-my-zsh-save')
     if os.path.exists(zsh_dir):
-        raise RuntimeError(red('You already have Oh My Zsh installed. Remove {0} before install.'.format(zsh_dir)))
+        print green('\tBacking up folder: {0} --> {1}'.format(cyan(zsh_dir), cyan(backup)), True)
+        if os.path.exists(backup):
+            shutil.rmtree(backup)
+        shutil.move(zsh_dir, backup)
 
     try:
         # clone zsh repo
         cmd = 'git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git {0}'.format(zsh_dir)
-        print yellow(check_output(shlex.split(cmd), preexec_fn=init_child_process), True)
+        check_output(shlex.split(cmd), stderr=subprocess.STDOUT, preexec_fn=init_child_process)
 
-        # link the config files
-        link_files(home)
+        target = os.path.join(current, '.zshrc')
+        zshrc = os.path.join(home, '.zshrc')
+        print green('\tSymlinking file: {0} --> {1}'.format(cyan(zshrc), cyan(target)), True)
+        check_output(shlex.split('ln -sf {0} {1}'.format(target, zshrc)))
 
         # change the shell
         if 'zsh' not in os.environ['SHELL']:
             os.system('chsh -s $(grep /zsh$ /etc/shells | tail -1)')
     except CalledProcessError as e:
-        print '{0}\n{1}'.format(red(e, True), yellow(e.output, True))
+        print '{0}\n{1}'.format(red(e, True), green(e.output, True))
+    finally:
+        print
 
 
-def link_files(home):
+def install_vim():
+    print green('Installing: {0}'.format(red('vim', True)), True)
+
+    home = os.getenv('HOME')
+    if not is_program_installed('vim'):
+        install_program('vim')
+
+    vim_dir = os.path.join(home, '.vim')
+    backup = os.path.join(home, '.vim-save')
+    if os.path.exists(vim_dir):
+        print green('\tBacking up folder: {0} --> {1}'.format(cyan(vim_dir), cyan(backup)), True)
+        if os.path.exists(backup):
+            shutil.rmtree(backup)
+        shutil.move(vim_dir, backup)
+
+    vimrc = os.path.join(home, '.vimrc')
+    backup = os.path.join(home, '.vimrc-save')
+    if os.path.exists(vimrc):
+        print green('\tBacking up file: {0} --> {1}'.format(cyan(vimrc), cyan(backup)), True)
+        if os.path.exists(backup):
+            os.remove(backup)
+        shutil.move(vimrc, backup)
+
+    try:
+        # clone zsh repo
+        cmd = 'git clone https://github.com/timss/vimconf.git {0}'.format(vim_dir)
+        # print green(check_output(shlex.split(cmd), stderr=subprocess.STDOUT))
+        check_output(shlex.split(cmd), stderr=subprocess.STDOUT, preexec_fn=init_child_process)
+
+        target = os.path.join(vim_dir, '.vimrc')
+        vimrc = os.path.join(home, '.vimrc')
+        print green('\tSynlinking file: {0} --> {1}'.format(cyan(vimrc), cyan(target)), True)
+        check_output(shlex.split('ln -sf {0} {1}'.format(target, vimrc)))
+
+    except CalledProcessError as e:
+        print '{0}\n{1}'.format(red(e, True), green(e.output, True))
+    finally:
+        print
+
+
+def link_files():
+    filenames = ('.bashrc', '.dircolors', '.inputrc', '.profile', '.Xdefaults', '.zshrc')
+    print green('Symlinking files: {0}'.format(red(filenames, True)), True)
+
+    home = os.getenv('HOME')
     current = os.path.abspath(os.path.dirname(__file__))
-    for filename in os.listdir(current):
-        if not fnmatch.fnmatch(filename, '.*') or filename in ('.git', '.gitignore', '.idea'):
-            continue
 
-        full_filename = os.path.join(home, filename)
-        if os.path.exists(full_filename):
-            msg = 'Configuration file exists: {0}, backing it up to: {1}'
-            print yellow(msg.format(blue(full_filename, True), blue(full_filename + '.save', True)), True)
-            shutil.move(full_filename, full_filename + '.save')
+    for filename in filenames:
+        target = os.path.join(home, filename)
+        if os.path.exists(target):
+            print green('\tBacking up file: {0} --> {1}'.format(cyan(target), cyan(target + '-save')), True)
+            shutil.move(target, target + '-save')
 
-        print yellow(
-            'Linking file: {0} --> {1}'.format(blue(os.path.join(current, filename), True), blue(full_filename)), True
-        )
-        os.symlink(os.path.join(current, filename), full_filename)
+        print green('\tSymlinking file: {0} --> {1}'.format(cyan(os.path.join(current, filename)), cyan(target)), True)
+        os.symlink(os.path.join(current, filename), target)
+        print
 
 
 if __name__ == '__main__':
+    install_vim()
     install_ohmyzsh()
+    link_files()
+    print green('Finished.', True)
