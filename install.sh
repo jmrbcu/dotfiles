f7c815777f9be8e8cdaea5da63eb3c60bfa8deb3
Jose Rodriguez <jmrbcu@gmail.com>
2021-03-16 00:56:52 -0500

    Fixed issue changin the shell

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ install-zsh() {
     # change the shell if is not already "zsh"
     TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
     if [[ "$TEST_CURRENT_SHELL" != "zsh" ]]; then
-        sudo chsh -s $(grep /zsh$ /etc/shells | tail -1)
+        sudo chsh -s $(grep /zsh$ /etc/shells | tail -1) $USER
     fi
 }
 
