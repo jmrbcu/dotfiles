5cd5bd208e4b879c51b9b6a75b1b205dbe6733fc
Jose Rodriguez <jmrbcu@gmail.com>
2022-09-06 15:59:02 -0400

    Fixed issue with less wrapper aborting

--- less.sh
+++ less.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-set -eE
 
 LESSPIPE=$(command -v src-hilite-lesspipe.sh)
 if [[ -z "$LESSPIPE" && -f /usr/share/source-highlight/src-hilite-lesspipe.sh ]]; then
-    LESSPIPE=/usr/share/source-highlight/src-hilite-lesspipe.sh
+  LESSPIPE=/usr/share/source-highlight/src-hilite-lesspipe.sh
+else
+  LESSPIPE=cat
 fi
 
-if (( `stat -f %z $1` < `expr 10*1024*1024` )); then
-  test -n "$LESSPIPE" && $LESSPIPE "$1" || cat "$1"
-else
-  cat $1
+if [[ -z "$1" ]]; then
+  exit
 fi
+
+if (( `stat -f %z $1` > `expr 10*1024*1024` )); then
+  LESSPIPE=cat
+fi
+
+$LESSPIPE "$1"
